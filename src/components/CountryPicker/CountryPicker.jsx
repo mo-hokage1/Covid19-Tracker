6fada97194255d4154799b36759a62908fbda0ae
mohamed <mqadir358@gmail.com>
2022-11-14 17:09:22 +0300

    first commit

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./CountryPicker.module.css"
+
+const CountryPicker = () => {
+    return (
+            <h1 className={styles.CountryPicker}>Country Picker</h1>
+    )
+}
+
+export default CountryPicker;
